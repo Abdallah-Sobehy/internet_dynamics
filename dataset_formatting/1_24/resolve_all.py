c24b4384e471974fe98a2c0aa1766e30d6ef8a2f
Abdallah Sobehy <a-sobehy@hotmail.com>
2016-02-08 21:28:53 +0100

    Update resolve all file, edges are resolved, only one star future edges can be resolved.

--- dataset_formatting/1_24/resolve_all.py
+++ dataset_formatting/1_24/resolve_all.py
@@ -29,27 +29,61 @@ def get_tuples_for_edge(edge,max_tuples):
 def combine_replace(edge, replacements, l12_r, ips_to_edge):
 	if len(replacements) == 0 : return
 	else:
+		# List of the final ips to replace stars
+		final_ips = []
 		replacing_edges = []
 		ip1 = replacements[0][0]
+		final_ips.append(ip1)
+		# Loop all ips after the first one (already saved in ip1)
 		for i in xrange(1, l12_r+1):
-			print 'in index ', i 
 			for l in replacements:
-				print 'checking one list'
+				# If a non star ip then add corresponding edge
 				if l[i] != '-1':
-					# print 'hey roger',(ip1+l[i]) 
 					if (ip1+l[i]) in ips_to_edge:
-						print 'adding edge ', ips_to_edge[ip1+l[i]]
 						replacing_edges.append(ips_to_edge[ip1+l[i]])
+						final_ips.append(l[i])
 						ip1 = l[i]
 						break
-					else: 	
+					else: # if not found in the dictionary, mongodb will be called for cross checking	
+						print '*******Cross checking Ip\'s ', ip1, '->' , l[i] 
 						record_tmp = c_dataset.find_one({"ip1": ip1, "ip2": l[i]})
 						if record_tmp != None:
+							print 'Cross checking succeeded'
 							replacing_edges.append(str(record_tmp['edge']))
-							ip1 = str(record_tmp['edge'])
+							ips_to_edge[ip1 + str(record_tmp['ip2'])] = str(record_tmp['edge'])
+							final_ips.append(str(record_tmp['ip2']))
+							ip1 = str(record_tmp['ip2'])
 							break
-		
+						
+		print 'final replacing_ips : ', final_ips
 		print 'final replacing_edges : ', replacing_edges
+		# Call future_replace function only when more than one list was used in resolving process
+		if len(replacements) > 1:
+			future_replace(final_ips,replacements, ips_to_edge)
+## 
+# Returns the number of stars in a list of ips
+# @param replacing_ips list of ips 
+def get_star_num(replacing_ips):
+	star_count = 0
+	for i in xrange(1,len(replacing_ips)):
+		if replacing_ips[i] == '-1':
+			star_count += 1
+	return star_count
+
+##
+# Uses the final replacing ips which is formed by combining replacing ips to resolve one star edges in one of the replacing lists
+# @param final list with final ips afer combining different lists
+# @param replacements_list a list that was used to compute the final ips
+def future_replace(final, replacements_list, ips_to_edge):
+	for sub in replacements_list:
+		# First and last indces are not considered as they never contain a star
+		for i in xrange(1, len(final) - 1):
+			# Replace if the subreplacement has one star edfe and the final has a known ip, and the ips before and after the star is the same for both
+			if sub[i] == '-1' and sub[i-1] != '-1' and sub[i+1] != '-1' and final[i] != '-1' and final[i-1] != '-1' and final[i+1] != '-1' and sub[i-1] == final[i-1] and sub[i+1] == final[i+1]:
+				print 'sub replacing ips: ', sub
+				print 'future replace of [ ', ips_to_edge[sub[i-1]+sub[i+1]],' ]', sub[i-1], ' -> ' , sub[i+1]
+				print 'By: [ ', ips_to_edge[final[i-1]+final[i]],' ]', final[i-1], ' -> ' , final[i]
+				print 'And: [ ', ips_to_edge[final[i]+final[i+1]],' ]', final[i], ' -> ' , final[i+1]
 
 
 
@@ -57,9 +91,8 @@ client = pm.MongoClient()
 db = client.dataset
 c_dataset = db.dataset_all_1_24
 
-total_edges_checked = 50
-start_line = 0 # line to start reading the edges from
-edges_checked = 1 # Number of edges checked for resolving
+total_edges_checked = 2 # Number of edges to be checked checked for resolving
+edges_checked = 0 # Edges checked for resolving so far
 edges_resolved = 0 # Number of edges successfully resolved
 start_time = time.time()
 Edges = genfromtxt('Edges_1_24.csv', delimiter=';', skip_header=0, skip_footer=0)
@@ -71,61 +104,59 @@ f = open('edges_resolved.csv', "a")
 f.write("edge;replacements\n")
 
 # Loop Edges to be resolved in ascending order of the number of stars
-edge_l12 = 2
-j = 0
+edge_l12 = 3
+k = 0
 while edge_l12 > 1:
-    if j == total_edges_checked: break
+	# Condition to check only specified number of edges
+    if k == total_edges_checked: break
     file_name = 'Edges_diff_'+str(edge_l12)+'.csv'
-    print 'resolving edges with l12 = ', edge_l12 
+    print '//////////////////////////\nresolving edges with l12 = ', edge_l12 
     # Loop all the edges in the edges file where only edges with one star are stored
     with open('./Edges_1_24_diff/'+file_name, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
         for row in reader:
             # To stop resolving when total_edges_checked is reached
-            if j == total_edges_checked: break
-            else: j+= 1
+            if k == total_edges_checked: break
+            else: k+= 1
             e_resolve = row[0]
             ip1_r = row[1]
             ip2_r = row[2]
             l12_r = int(row[3])
             resolved = False
-            print '========================'
-            print 'Edge to be resolved [ ', e_resolve,' ]', ip1_r, ' -> ' , ip2_r 
+            print '========================\nEdge to be resolved [ ', e_resolve,' ]', ip1_r, ' -> ' , ip2_r 
             # Get tuples pointing at the same edge.
             tuples = get_tuples_for_edge(e_resolve,5)
             print 'number of found tuples = ', len(tuples)
+            # List of lists to store ips that could replace the [ip1,*,*....,ip2] of the edge to be resolved for each tuple
             replacements_list = []
             # Loop tuples
+            # dictionary to store key: ip1+ip2 to their edge id for ease of access
+            ips_to_edge = {}
             for t in tuples:
                 print '\ntuple: ', t, 'h1 = ', str(Tuples[t,3])[:-2], 'h2 = ', str(Tuples[t,4])[:-2]
-                if str(Tuples[t,4])[:-2] == 31:
+                if str(Tuples[t,4])[:-2] == '31':
                 	print 'Will not examine tuple with h2 == 31.'
-                	continue
-                # List to store replacing edges
-                replacing_edges = []
+                	break
+                # ips that could replace the [ip1,*,*....,ip2]
                 replacing_ips = []
-                ips_to_edge = {}
                 # add the ip1 of the current edge because it has to be a part of the replacement
                 replacing_ips.append(ip1_r)
                 # The index of the last non star ip found
                 known_ip_index = 0
-                # Loop number of maximum possible replacing edges (if all stars resolved)= l12
+                # Loop for each ip of the edge (including stars) = l12
                 h1_index = str(Tuples[t,3])[:-2]
                 h1_h2_diff = 1
                 for j in xrange(l12_r):
-                    print 'searched: h1 = ', h1_index, ' h2 = ', str(int(h1_index)+h1_h2_diff), ' ip1 must be = ' , replacing_ips[known_ip_index]
-                    # find tuple with h1 = h1_r and h2 = h1_r + 1 [store its edge] if found the h1 for next round is +=1 and h2 should be reset to h1_r + 1 (because it might be incremented by th next line)
+                    # print 'searched: h1 = ', h1_index, ' h2 = ', str(int(h1_index)+h1_h2_diff), ' ip1 must be = ' , replacing_ips[known_ip_index]
+                    # find tuple with h1 = h1_r and h2 = h1_r + 1 initially, then both would change according to findings
                     record_tmp = c_dataset.find_one({"src": str(Tuples[t,1])[:-2], "dst": str(Tuples[t,2])[:-2], "h1": h1_index , "h2": str(int(h1_index)+h1_h2_diff)})
                     # If not found h1 stays the same but the h2 is moved one step further from h2
-                    if record_tmp != None:
-                    	print 'edge to be tested for validity [ ', record_tmp['edge'] ,' ]', record_tmp['ip1'], ' -> ' , record_tmp['ip2'] 
                     if record_tmp == None or record_tmp['ip1'] != replacing_ips[known_ip_index]:
                         replacing_ips.append('-1')
                         h1_h2_diff +=1
                         continue
                     else: # Add the replacing edge to the list and move h1_index and reset h1_h2_diff
-                        replacing_edges.append(record_tmp['edge'])
-                        print 'replacing Edge [ ', record_tmp['edge'] ,' ]', record_tmp['ip1'], ' -> ' , record_tmp['ip2'] 
+                        # print 'replacing Edge [ ', record_tmp['edge'] ,' ]', record_tmp['ip1'], ' -> ' , record_tmp['ip2'] 
                         replacing_ips.append(str(record_tmp['ip2']))
                         # Update ip to edge dictionary
                         ips_to_edge[str(record_tmp['ip1'])+str(record_tmp['ip2'])] = str(record_tmp['edge'])
@@ -140,25 +171,24 @@ while edge_l12 > 1:
                 	# call combine_replace function with one list in replacements_list
                 	replacements_list.append(replacing_ips)
                 	resolved = True
-                	print 'ips_to_edge', ips_to_edge
+                	# print 'ips_to_edge', ips_to_edge
                 	combine_replace(e_resolve, replacements_list, l12_r, ips_to_edge)
+                	edges_resolved += 1
                 	break
                 else:
                 	print 'possible replacing ips: ', replacing_ips
-                	replacements_list.append(replacing_ips)
-            if resolved == False:
-				print 'Replacements from different tuples: ', replacements_list	
-				combine_replace(e_resolve, replacements_list, l12_r, ips_to_edge)        
-                # Check the ip2 of the last edge in the replacements list must be = to ip2_r otherwise not accepted
-                # If true the list is saved in replacements_list with unknown edges saved as -1 
-                # A condition to avoid unioning lists is if there is no unknown edges == all stars resolved so no need for making union
-            # If none of the lists has all stars resolved we call a function to union them
-                # 2 nested loops
-                    # in a list if an unknown edges is encountered, the same index is searched in all other lists
-                    # If a known edge is found in that index the ips are checked to see if they have one in common
-                    # If one can replace this unknown edge the looping continues from the list with the found edge
-            # Finally the resolved 
-    
+                	# Possible replacements are only added for combining if the last ip == ip2 of edge to be resolved
+                	# and at least one star is replaced by an id
+                	if replacing_ips[l12_r] == ip2_r and get_star_num(replacing_ips) < l12_r - 1:
+                	   	replacements_list.append(replacing_ips)
+                	else: print 'Excluded from combine_replace.'
+            # If none of the lists has all stars resolved, call combine_replace function to union them
+            if resolved == False and len(replacements_list) > 0:
+				# print 'Replacements from different tuples: ', replacements_list	
+				combine_replace(e_resolve, replacements_list, l12_r, ips_to_edge)
+				edges_resolved += 1 
+            edges_checked +=1
+            print 'Edges resolved: ', edges_resolved , '/' , edges_checked
     # Update the edge_l12 with the next value to be resolved
     # Ignore edge with stars 17,18,19,20 as they do not exist
     if edge_l12 == 21:
